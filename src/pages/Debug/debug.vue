c29cda053aa82dec12f2cf427de4ea67fc89830d
abc088_6kAX_code <86451477+abc-0886kAX-code@users.noreply.github.com>
2023-04-23 17:51:19 +0800

    :construction:
    :construction:

--- src/pages/Debug/debug.vue
+++ src/pages/Debug/debug.vue
@@ -1,7 +1,20 @@
+<!--
+ * @FilePath: \fx-mentougou\src\pages\Debug\debug.vue
+ * @Author: zhangxin
+ * @Date: 2023-04-14 10:21:12
+ * @LastEditors: zhangxin
+ * @LastEditTime: 2023-04-23 17:50:59
+ * @Description:
+-->
 <script setup></script>
 
 <template>
-    <div></div>
+    <div class="debug"></div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.debug {
+    width: 100%;
+    height: 100%;
+}
+</style>
