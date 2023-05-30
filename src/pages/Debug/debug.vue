4dab4f0713f0c01ac55fc0fdd134f7da5f7ab750
abc088_6kAX_code <86451477+abc-0886kAX-code@users.noreply.github.com>
2023-05-30 14:54:18 +0800

    :-1:
    :-1:

--- src/pages/Debug/debug.vue
+++ src/pages/Debug/debug.vue
@@ -3,16 +3,10 @@
  * @Author: zhangxin
  * @Date: 2023-04-14 10:21:12
  * @LastEditors: zhangxin
- * @LastEditTime: 2023-05-30 13:55:17
+ * @LastEditTime: 2023-05-30 14:53:14
  * @Description:
 -->
-<script setup>
-import { toGeoJSON } from "shp-geojson";
-
-toGeoJSON("https://mhsw.ytxd.com.cn:8081/mh/file/historical.zip").then((geojson) => {
-    console.log(geojson);
-});
-</script>
+<script setup></script>
 
 <template>
     <div class="debug"></div>
